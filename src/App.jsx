c2487042b4fbd61c3f75b1e422bf503462e59dcf
Liyema Mkebe <liyema.mkebe15@gmail.com>
2026-08-25 16:07:00 +0200

    Initailizing repo + React and Tailwind base

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Navbar from './components/Navbar.jsx'
+
+function Page() {
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default function App() {
+
+  return (
+    <>
+      <Page />
+    </>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function PageNav() {
+    return (
+        <h1 className="text-[65px] font-bold">Hello, Falcon!</h1>
+    )
+}
+
+export default function Navbar() {
+    return (
+        <PageNav />
+    )
+}
